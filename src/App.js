a35def589d112afa05eac6556855efd0ca0537f2
balayokesh <balayokeshmani@gmail.com>
2023-01-07 22:19:25 +0530

    Completed front page design

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
 import { makeStyles, Container, Typography, Button } from '@material-ui/core';
-import {useHistory} from 'react-router-dom';
 
 const useStyles = makeStyles({
-	mainDiv: {
-		height: '100vh',
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	innerDiv: {
-		display: 'flex',
-		flexDirection: 'column',
-		textAlign: 'center',
-	},
-	link: {
-		color: '#fff',
-		textDecoration: 'none',
+	container: {
+		display: "flex",
+		flexDirection: "column",
+		height: "100vh",
+		justifyContent: "center",
+		alignItems: "center",
 	}
 });
 
 const App = () => {
-
-	let history = useHistory()
-	const handleClick = () => {
-		history.push('/game');
-	}
-
 	const classes = useStyles();
 	return (
-		<Container className={classes.mainDiv}>
-			<Container className={classes.innerDiv}>
-				<Typography variant='h3'>
-					Let's play XoXo
-				</Typography>
-				<Button variant='contained' onClick={handleClick}>
-					Play Game
-				</Button>
-			</Container>
+		<Container className={classes.container}>
+			<Typography variant='h2'>
+				XOXO
+			</Typography>
+			<Button variant='contained' color='primary' onClick={()=> window.location.href='/game'}>
+				Play Game
+			</Button>
 		</Container>
 	);
 }
